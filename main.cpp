cc66348d62c0ce917adce3832f5c218c38eae2de
Jakub <jakub.zojdzik@gmail.com>
2021-03-22 17:27:51 +0100

    Starting!
    Added main.cpp file

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Python nie taki zly jakby ci sie moglo wydawac\n";
+	cout << "Serio musisz kiedys napisac jakis projekt to sie przekonasz\'n";
+}
