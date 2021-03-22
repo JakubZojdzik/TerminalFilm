85a242ca49eb90b4d40a679b4f33b5e796d73bec
Jakub <jakub.zojdzik@gmail.com>
2021-03-22 20:03:27 +0100

    rgbToAnsi funcion added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
+string rgbToAnsi(int r, int g, int b)
+{
+	return ("\u001b[48;2;" + to_string(r) + ";" + to_string(g) + ";" + to_string(b) + "m");
+}
+
 int main()
 {
-	cout << "Python nie taki zly jakby ci sie moglo wydawac\n";
-	cout << "Serio musisz kiedys napisac jakis projekt to sie przekonasz\'n";
+	cout << rgbToAnsi(255, 0, 0) << "  " << rgbToAnsi(0, 255, 0) << "  " << rgbToAnsi(0, 0, 255) << "  " << rgbToAnsi(255, 255, 255) << "  " << rgbToAnsi(0, 0, 0) << "  \n";
 }
