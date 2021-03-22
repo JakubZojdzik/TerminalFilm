96e107539d58b242d85e71fffcf505319e0478ac
Jakub <jakub.zojdzik@gmail.com>
2021-03-22 20:20:42 +0100

    Pixel size added

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@ string rgbToAnsi(int r, int g, int b)
 	return ("\u001b[48;2;" + to_string(r) + ";" + to_string(g) + ";" + to_string(b) + "m");
 }
 
+
 int main()
 {
+	const spaceWidth = 32; 	//pixels
+	const spaceHeight = 64; //pixels
 	cout << rgbToAnsi(255, 0, 0) << "  " << rgbToAnsi(0, 255, 0) << "  " << rgbToAnsi(0, 0, 255) << "  " << rgbToAnsi(255, 255, 255) << "  " << rgbToAnsi(0, 0, 0) << "  \n";
 }
