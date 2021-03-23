a2b8ad88f40d74af0d3cd17e68ec7bfb044d3833
Jakub <jakub.zojdzik@gmail.com>
2021-03-23 13:09:27 +0100

    Colorizing functions
    Working function colorizing background and char

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 using namespace std;
 
-string rgbToAnsi(int r, int g, int b)
+string rgbToAnsiBg(int r, int g, int b)
 {
 	return ("\u001b[48;2;" + to_string(r) + ";" + to_string(g) + ";" + to_string(b) + "m");
 }
 
+string rgbToAnsiChar(int r, int g, int b){
+	if(r < 75)
+		r = 0;
+	else
+		r = (r-35)/40 * 36;
+
+	if(g < 75)
+		g = 0;
+	else
+		g = (g-35)/40 * 6;
+
+	if(b < 75)
+		b = 0;
+	else
+		b = (b-35)/40;
+
+	return "\u001b[38;5;" + to_string(r+g+b+16) + "m";
+
+}
 
 int main()
 {
-	const spaceWidth = 32; 	//pixels
-	const spaceHeight = 64; //pixels
-	cout << rgbToAnsi(255, 0, 0) << "  " << rgbToAnsi(0, 255, 0) << "  " << rgbToAnsi(0, 0, 255) << "  " << rgbToAnsi(255, 255, 255) << "  " << rgbToAnsi(0, 0, 0) << "  \n";
+	const int spaceWidth = 32; 	//pixels
+	const int spaceHeight = 64; //pixels
+	string code = "";
+	cout << rgbToAnsiChar(255, 255, 255) << " - ALA MA KOTA - "  << rgbToAnsiChar(255, 0, 0) << " - ALA MA KOTA - "  << rgbToAnsiChar(0, 255, 0) << " - ALA MA KOTA" << endl;
 }
