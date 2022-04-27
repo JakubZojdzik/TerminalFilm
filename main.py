9a1b8af74c9fafdb5d062f1da83e06cb5130532f
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-04-27 22:06:58 +0200

    xd

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import cv2
 import time
+import os
 
 def colored(r, g, b, text):
     return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(r, g, b, text)
 
 video_name = "input.mp4"
 cap = cv2.VideoCapture(video_name)
-width = 165 # small - 165x82, or high resolution - 234x130
-height = 82
-fps = 35 
+width = 131 # S - 132x65, M - 165x82, L - 234x130
+height = 64
+fps = 35
 speed = 2 # with small resolution - 2/3, with high resolution - 3/4 is recomended
 pxl = "█"
 print("\033c", end="")
@@ -16,7 +17,11 @@ for k in range(1000):
     for _ in range(speed):
         ret, frame = cap.read()
     if ret:
-        print("\033[%d;%dH" % (0, 0), end='')
+        #os.system("clear")
+        #print(chr(27)+'[2j')
+        #print('\033c')
+        print('\x1bc')
+        #print("\033[%d;%dH" % (0, 0), end='')
         frame = cv2.resize(frame, (width, height), interpolation =cv2.INTER_AREA)
         s = ""
         for i in range(int(height/2)):
